6b19f73276e1bb077132345d7b2a554b4c0630bc
tkn1223 <taka12230000@gmail.com>
2025-06-22 20:35:32 +0900

    ユーザーモデルの追加

---
+++ spec/requests/site_layout_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "SiteLayout", type: :request do
+    describe "Get / (root_path)" do
+        before { get root_path }
+        
+        it "renders the home template" do
+            expect(response).to render_template(:home)
+        end
+
+        it "includes correct links" do
+            expect(response.body).to include("href=\"#{root_path}\"")
+            expect(response.body).to include("href=\"#{help_path}\"")
+            expect(response.body).to include("href=\"#{about_path}\"")
+            expect(response.body).to include("href=\"#{contact_path}\"")
+            expect(response.body).to include("href=\"#{signup_path}\"")
+        end
+    end
+end
