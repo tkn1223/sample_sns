130109df44757141a84dac73060c4f9a13f4b503
tkn1223 <taka12230000@gmail.com>
2025-06-23 07:11:52 +0900

    ユーザー情報の検証（存在、長さ、フォーマット）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  it "ユーザー情報がバリデーションを満たしているか確認" do
+    expect(@user).to be_valid
+  end
+
+  it "nameの存在性を確認" do
+    @user.name = ""
+    expect(@user).not_to be_valid
+  end
+
+  it "emailの存在性を確認" do
+    @user.email = ""
+    expect(@user).not_to be_valid
+  end
+
+  it "nameの文字数を確認" do
+    @user.name = "a" * 13
+    expect(@user).not_to be_valid
+  end
+
+  it "emailの文字数を確認" do
+    @user.email = "a" * 244 + "@example.com"
+    expect(@user).not_to be_valid
+  end
+
+  it "emailのよくあるダメパターンを確認" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_addresses|
+      @user.email = invalid_addresses
+      expect(@user).not_to be_valid
+    end
+  end
 end
