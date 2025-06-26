9e6a71be33f1a854cc236344a1bd32f3d5850bd4
noel16 <furuya@noel-ltd.com>
2025-06-26 18:44:36 +0900

    正しくユーザー登録できる処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存が成功したときの処理
+      flash[:success] = "ユーザー登録が完了しました"
+      redirect_to @user
     else
       render "new", status: :unprocessable_entity
     end
--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -20,4 +20,24 @@ RSpec.describe "UsersSignups", type: :request do
       expect(response.body).to include('class="form-control"')
     end
   end
+
+  describe "POST /users_signups" do
+    it "正しくログインできるか確認" do
+      get signup_path
+      expect {
+        post users_path, params: {
+          user: {
+            name: "Exampleuser",
+            email: "user@example.com",
+            password: "password12",
+            password_confirmation: "password12"
+          }
+        }
+      }.to change(User, :count).by(1)
+
+      follow_redirect!
+      expect(response.body).to include("Exampleuser")
+      expect(flash[:success]).to eq("ユーザー登録が完了しました")
+    end
+  end
 end
