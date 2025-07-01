a52b17145c3b021174c9a2ac929515fea28a445d
noel16 <furuya@noel-ltd.com>
2025-07-01 13:09:48 +0900

    ログイン可否でヘッダーの表示を変更させる

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,14 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    # ユーザーがログインしていればtrue、その他ならfalseを返す
+    def logged_in?
+        !current_user.nil?
+    end
 end
