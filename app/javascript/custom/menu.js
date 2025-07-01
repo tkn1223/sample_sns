0025300526aac112f455a7f4a26cde9c36de1e34
noel16 <furuya@noel-ltd.com>
2025-07-01 18:14:28 +0900

    トグルの開閉処理を実装

---
+++ app/javascript/custom/menu.js
@@ -0,0 +1,16 @@
+// メニュー操作
+
+// トグルリスナーを追加してクリックをリッスンする
+function addToggleListener(selected_id, menu_id, toggle_class) {
+  let selected_element = document.querySelector(`#${selected_id}`);
+  selected_element.addEventListener("click", function(e) {
+      e.preventDefault();
+      let menu = document.querySelector(`#${menu_id}`);
+      menu.classList.toggle(toggle_class);
+  })
+}
+
+document.addEventListener("turbo:load", function() {
+  addToggleListener("hamburger", "navbar-menu", "collapse");
+  addToggleListener("account", "dropdown-menu", "active");
+});
