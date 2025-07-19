23b7cf6a692f9369f92acd397f90d8ed8d28dde1
tkn1223 <taka12230000@gmail.com>
2025-07-19 22:31:54 +0900

    パスワードの再設定機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
   get "sessions/new"
   root "static_pages#home"
   get "/help",       to: "static_pages#help"
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
   delete "/logout",  to: "sessions#destroy"
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
